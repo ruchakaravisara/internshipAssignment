be5a2f4bdfd80309c9f7949c3c3ed87a455e7c54
Ruchaka Ravisara <ruchakaravisara1@gmail.com>
2024-03-15 20:04:06 +0530

    first commit

---
+++ backend/src/main/java/lk/texonic/spring/WebAppInitializer.java
@@ -0,0 +1,17 @@
+package lk.texonic.spring;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class WebAppInitializer {
+    public static void main(String[] args) {
+        SpringApplication.run(WebAppInitializer.class);
+    }
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
+}
